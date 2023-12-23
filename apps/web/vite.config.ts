df15835fcf81ff73af775d22723d84a95c9ca592
Jeremy Frank <18402381+AskingQuestions@users.noreply.github.com>
2023-12-22 20:25:11 -0500

    init structure

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,16 +2,23 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}']
-  },
-  optimizeDeps: {
-    include: ['@repo/ui'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/@repo-ui/, /node_modules/],
-    },
-  },
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	optimizeDeps: {
+		include: ['@repo/ui']
+	},
+	preview: {
+		port: 3400
+	},
+	server: {
+		port: 3400,
+		strictPort: true
+	},
+	build: {
+		commonjsOptions: {
+			include: [/@repo-ui/, /node_modules/]
+		}
+	}
 });
